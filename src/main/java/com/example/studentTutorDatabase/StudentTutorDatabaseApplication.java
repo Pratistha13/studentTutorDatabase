e43c78a4cf7555ef899214069483870687bab47e
P1748 <p1748@student.jamk.fi>
2020-11-24 10:45:08 +0200

    added new files

---
+++ src/main/java/com/example/studentTutorDatabase/StudentTutorDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentTutorDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTutorDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTutorDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentTutorDatabase/StudentTutorDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentTutorDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTutorDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
